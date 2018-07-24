1062ec1b03623274f57fae397b2abc84cb1a9217
C1ff <c1ff@alandmoore.com>
2018-07-24 13:54:25 -0500

    Organize main.py to prepare for shaders.

--- main.py
+++ main.py
@@ -24,27 +24,31 @@ world = worldGen.worldGen(10)
 
 # Temporary line to test world rendering.
 world[1][1] = 5
-
-print(world)
-
-# Initialize Physics Engine. We use pybullet.DIRECT since we are not using pybullet's GUI rendering system.
-physicsClient = pybullet.connect(pybullet.DIRECT)
-pybullet.setGravity(0,0,-10)
-
-# Create a plane to stop our cubes from falling into infinity. (Temporary?)
-plane = pybullet.createCollisionShape(pybullet.GEOM_PLANE)
-pybullet.createMultiBody(0,plane,-1,[0,0,-9])
-
-# Initialize a Pygame window showing OpenGL
-pygame.init()
 display = (1200, 720)
-pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-# Allow keys to be held down.
-pygame.key.set_repeat(1, 2)
+def init_libs():
+	"""Initialize Pybullet and Pygame. Turn on GL's depth test and make the sky blue."""
+	physicsClient = pybullet.connect(pybullet.DIRECT)
+	pybullet.setGravity(0,0,-40)
+	
+	pygame.init()
+	pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+	pygame.key.set_repeat(1, 2)
+	
+	glEnable(GL_DEPTH_TEST)
+	glClearColor(0.5, 0.6, 1.0, 0.0);
+	
+def setup_world():
+	"""Sets up the basic debug world."""
+	plane = pybullet.createCollisionShape(pybullet.GEOM_PLANE)
+	pybullet.createMultiBody(0,plane,-1,[0,0,-9])
 
-# gluLookAt takes 9 arguments, the camera position, the lookat position and the up vector. (Just set that to all zeroes except for a 1 for the position that is upwards)
-# gluLookAt multiplies the current vector rather than changing the camera vector because PyOpenGL is stupid. Use glLoadIdentity() to stop this.
+	# Later on my plan is to just generate a world. For now, we need some debug cubes.
+	cubes.append(cubeRender.createCube([0,12,0], 1, [0,0,45]))
+	cubes.append(cubeRender.createCube([4,4,6], 1, [0,0,0]))
+	cubes.append(cubeRender.createCube([4,5.9,9], 2, [0,0,0]))
+	
+	boxestodelete = worldGen.resetWorldBoxes(8, -9, world) # We run this once to initiate the first collision boxes.
 
 def reset_camera():
 	"""Resets the camera to the start position. Returns Yaw and Camera Position."""
@@ -55,26 +59,37 @@ def reset_camera():
 	cameray = 1
 	cameraz = -2
 	
+	# gluLookAt takes 9 arguments, the camera position, the lookat position and the up vector.
+	# (Just set the up vector to all zeroes except for a 1 for the axis that is upwards)
+	# gluLookAt also multiplies the "current vector" rather than changing the camera vector because PyOpenGL is stupid.
+	# Use glLoadIdentity() to stop this.
+	
 	glLoadIdentity()
 	gluPerspective(45, (float(display[0])/float(display[1])), 0.1, 100.0)
 	gluLookAt(camerax,cameray,cameraz, camerax+math.cos(yaw),cameray+math.sin(yaw),-4, 0,0,1)
 	return yaw, camerax, cameray, cameraz
-
-yaw, camerax, cameray, cameraz = reset_camera()
 	
 
-glEnable(GL_DEPTH_TEST)
+def render_loop(vertex_data, color_data):
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+	
+	glBegin(GL_TRIANGLES)
+	
+	i = 0
+	for vertex in vertex_data:
+		glColor3fv(color_data[i])
+		glVertex3fv(vertex)
+		i+=1
+	
+	glEnd()
+	
 
-cubes.append(cubeRender.createCube([0,12,0], 1, [0,0,45]))
-cubes.append(cubeRender.createCube([4,4,6], 1, [0,0,0]))
-cubes.append(cubeRender.createCube([4,5.9,9], 2, [0,0,0]))
-
-glClearColor(0.5, 0.6, 1.0, 0.0);
+init_libs()
+setup_world()
+yaw, camerax, cameray, cameraz = reset_camera()
 
 walkspeed = 0.5
 turnspeed = 0.03
-
-boxestodelete = worldGen.resetWorldBoxes(8, -9, world) # We run this once to initiate the first collision boxes.
 
 while True:
 	for event in pygame.event.get():
@@ -98,6 +113,9 @@ while True:
 				yaw, camerax, cameray, cameraz = reset_camera()
 			if pressed_keys[pygame.K_q]:
 				boxestodelete = worldGen.resetWorldBoxes(8, -9, world, boxestodelete)
+			if pressed_keys[pygame.K_f]:
+				for cube in cubes:
+					pybullet.applyExternalForce(cube[0], -1, [0,0,100],[0,0,0],pybullet.LINK_FRAME)
 	
 	glLoadIdentity()
 	gluPerspective(45, (float(display[0])/float(display[1])), 0.1, 100.0)
@@ -127,21 +145,8 @@ while True:
 		for vertex in cubepoints:
 			render_vertices.append(vertex)
 			colors.append((0.5,0.5,0.5))
-	
 		
-	
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-	
-		
-	glBegin(GL_TRIANGLES)
-	
-	i = 0
-	for vertex in render_vertices:
-		glColor3fv(colors[i])
-		glVertex3fv(vertex)
-		i+=1
-	
-	glEnd()
+	render_loop(render_vertices, colors)
 	
 	# Empty Vertex List
 	render_vertices = []
