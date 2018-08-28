ed8c4f46a91afcdcba7fb7af602230a0e83286df
C1ff <c1ff@alandmoore.com>
2018-08-28 18:40:24 -0500

    World Gen improvement

--- world/worldGen.py
+++ world/worldGen.py
@@ -2,16 +2,36 @@ import random
 import pybullet
 import math
 
-def worldGen(size, amplitude=1):
+def worldGen(size, amplitude=3):
 	"""Temporary worldgen function. Will be replaced later with something
 	that actually looks nice. Takes size and noise amplitude and returns
 	world vertices for use in other functions."""
+	worlda = []
+	worldb = []
 	world = []
 	
-	for x in range(0,size):
-		world.append([])
-		for y in range(0,size):
-			world[x].append(random.randint(0,amplitude))
+	if size > 4:
+		for x in range(0,size): # I'd make these smaller heightmaps be... well... smaller, but then I just run into annoying list index out of range errors.
+			worlda.append([])
+			for y in range(0,size):
+				worlda[x].append(random.randint(0,amplitude))
+				
+		for x in range(0, size):
+			worldb.append([])
+			for y in range(0,size):
+				print(x,y, size/2, math.floor(y/2.0))
+				worldb[x].append(worlda[int(math.floor(x/2.0))][int(math.floor(y/2.0))] + random.randint(-amplitude/3,amplitude/3))
+		
+		for x in range(0, size):
+			world.append([])
+			for y in range(0,size):
+				world[x].append(worldb[int(math.floor(x/2.0))][int(math.floor(y/2.0))] + random.randint(-amplitude/4,amplitude/4))
+	elif size < 2:
+		print("WARNING: The worldsize entered is lower than the normal generator can accept! Using the legacy generator.")
+		for x in range(0, size):
+			world.append([])
+			for y in range(0,size):
+				world[x].append(random.randint(-amplitude,amplitude))
 	
 	return world
 
